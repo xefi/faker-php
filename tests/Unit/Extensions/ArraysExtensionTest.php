3f755e92623e2653e729e0a64d102bb2a6adf90e
Lucas GOSGNACH <l.gosgnach@skera.com>
2025-03-06 09:48:10 +0100

    Change Array test

--- tests/Unit/Extensions/ArraysExtensionTest.php
+++ tests/Unit/Extensions/ArraysExtensionTest.php
@@ -23,11 +23,11 @@ final class ArraysExtensionTest extends TestCase
         $this->assertEqualsCanonicalizing($elements, $this->testArray);
     }
 
-    public function testRandomKey(): void
+    public function testRandomKeyNumber(): void
     {
         $elements = [];
         for ($i = 0; $i < count($this->testArray); $i++) {
-            $elements[] = $this->faker->unique()->randomKey($this->testArray);
+            $elements[] = $this->faker->unique()->randomKeyNumber($this->testArray);
         }
 
         $this->assertEqualsCanonicalizing($elements, array_keys($this->testArray));
