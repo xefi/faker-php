031219734088be3376aa7934d62dbfa9447b1739
LixNew <129871802+LixNew2@users.noreply.github.com>
2025-10-13 11:14:35 +0200

    random timezone

--- src/Extensions/DateTimeExtension.php
+++ src/Extensions/DateTimeExtension.php
@@ -6,6 +6,38 @@ use DateTime;
 
 class DateTimeExtension extends Extension
 {
+    protected array $timezones = [
+        "America/Chicago",
+        "Europe/Moscow",
+        "Asia/Kolkata",
+        "Pacific/Auckland",
+        "Europe/Paris",
+        "Africa/Nairobi",
+        "America/New_York",
+        "Asia/Tokyo",
+        "Europe/London",
+        "America/Sao_Paulo",
+        "Australia/Sydney",
+        "Europe/Berlin",
+        "Asia/Dubai",
+        "America/Los_Angeles",
+        "Africa/Cairo",
+        "Europe/Amsterdam",
+        "Asia/Seoul",
+        "Pacific/Fiji",
+        "America/Mexico_City",
+        "Europe/Athens",
+        "Asia/Shanghai",
+        "Atlantic/Reykjavik",
+        "Africa/Johannesburg",
+        "Europe/Stockholm",
+        "Asia/Hong_Kong",
+        "Pacific/Honolulu",
+        "Europe/Warsaw",
+        "Australia/Perth",
+        "America/Toronto"
+    ];
+
     protected function formatTimestamp(DateTime|int|string $timestamp): int
     {
         if (is_int($timestamp)) {
@@ -27,5 +59,9 @@ class DateTimeExtension extends Extension
     public function timestamp(DateTime|int|string $fromTimestamp = '-30 years', DateTime|int|string $toTimestamp = 'now'): int
     {
         return $this->randomizer->getInt($this->formatTimestamp($fromTimestamp), $this->formatTimestamp($toTimestamp));
+    }
+
+    public function timezone() : string{
+        return $this->pickArrayRandomElement($this->timezones);
     }
 }
