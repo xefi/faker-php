bcfe6a1dbc5c5adfe4cdba2c582a4d49eb880c5d
Martin Soenen <martin.soenen@outlook.fr>
2025-10-23 22:12:52 +0200

    :arrow_up: Upgrade the phone extension

--- src/Extensions/PhoneExtension.php
+++ src/Extensions/PhoneExtension.php
@@ -8,18 +8,58 @@ class PhoneExtension extends Extension
 {
     use HasLocale;
 
-    protected array $formats = [
-        '+{d}{d}{d}{d}{d}{d}{d}{d}{d}',
-        '0{d}{d}{d}{d}{d}{d}{d}{d}{d}',
-        '{d}{d}{d}',
+    public string $defaultPrefix = '';
+
+    public string $indicator = '00';
+
+    protected array $landlineFormats = [
+        '{d}{d}{separator}{d}{d}{separator}{d}{d}{separator}{d}{d}{separator}{d}{d}',
+        '{d}{d}{separator}{d}{d}{separator}{d}{d}{separator}{d}{d}',
     ];
 
-    public function phoneNumber($format = null): string
+    protected array $cellFormats = [
+        '{d}{d}{separator}{d}{d}',
+    ];
+
+    public function phoneNumber($format = null, $separator = '', $prefix = null): string
     {
         if (!$format) {
-            $format = $this->pickArrayRandomElement($this->formats);
+            $format = $this->pickArrayRandomElement(rand(0, 1) ? $this->landlineFormats : $this->cellFormats);
         }
 
-        return $this->formatString($format);
+        while (($pos = strpos($format, '{separator}')) !== false) {
+            $format = substr_replace($format, $separator, $pos, 11);
+        }
+
+        if (is_null($prefix)) {
+            $prefix = $this->defaultPrefix;
+        }
+
+        return $prefix.$this->formatString($format);
+    }
+
+    public function cellPhoneNumber($separator = '', $prefix = null): string
+    {
+        return $this->phoneNumber(format: $this->pickArrayRandomElement($this->cellFormats), separator: $separator, prefix: $prefix);
+    }
+
+    public function landlinePhoneNumber($separator = '', $prefix = null): string
+    {
+        return $this->phoneNumber(format: $this->pickArrayRandomElement($this->landlineFormats), separator: $separator, prefix: $prefix);
+    }
+
+    public function indicatoredPhoneNumber($format = null, $separator = ''): string
+    {
+        return $this->phoneNumber(format: $format, separator: $separator, prefix: $this->indicator);
+    }
+
+    public function indicatoredCellPhoneNumber($separator = ''): string
+    {
+        return $this->cellPhoneNumber(separator: $separator, prefix: $this->indicator);
+    }
+
+    public function indicatoredLandlinePhoneNumber($separator = ''): string
+    {
+        return $this->landlinePhoneNumber(separator: $separator, prefix: $this->indicator);
     }
 }
--- tests/Unit/Extensions/PhoneExtensionTest.php
+++ tests/Unit/Extensions/PhoneExtensionTest.php
@@ -7,16 +7,6 @@ use Xefi\Faker\Extensions\PhoneExtension;
 
 final class PhoneExtensionTest extends TestCase
 {
-    protected array $formats = [];
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        $phoneExtension = new PhoneExtension(new Randomizer());
-        $this->formats = (new \ReflectionClass($phoneExtension))->getProperty('formats')->getValue($phoneExtension);
-    }
-
     public function testPhoneNumberReturnsString(): void
     {
         $number = $this->faker->phoneNumber();
@@ -25,13 +15,13 @@ final class PhoneExtensionTest extends TestCase
         $this->assertNotEmpty($number);
     }
 
-    public function testPhoneNumberWithoutFormat(): void
+    public function testPhoneNumberWithDefaultSettings(): void
     {
         for ($i = 0; $i < 100; $i++) {
             $number = $this->faker->phoneNumber();
 
             $this->assertMatchesRegularExpression(
-                '/^(\d{3}|\d{10}|\+\d{9})$/',
+                '/^(\d{4}|\d{8}|\d{10})$/',
                 $number
             );
         }
@@ -44,6 +34,213 @@ final class PhoneExtensionTest extends TestCase
 
             $this->assertMatchesRegularExpression(
                 '/^\d{3}-\d{3}$/',
+                $number
+            );
+        }
+    }
+
+    public function testPhoneNumberWithSeparator(): void
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $number = $this->faker->phoneNumber('{d}{d}{d}{separator}{d}{d}{d}', '-');
+
+            $this->assertMatchesRegularExpression(
+                '/^\d{3}-\d{3}$/',
+                $number
+            );
+        }
+
+        for ($i = 0; $i < 10; $i++) {
+            $number = $this->faker->phoneNumber('{d}{d}{d}{separator}{d}{d}{d}', '.');
+
+            $this->assertMatchesRegularExpression(
+                '/^\d{3}.\d{3}$/',
+                $number
+            );
+        }
+
+        for ($i = 0; $i < 10; $i++) {
+            $number = $this->faker->phoneNumber('{d}{d}{d}{separator}{d}{d}{d}', ' ');
+
+            $this->assertMatchesRegularExpression(
+                '/^\d{3} \d{3}$/',
+                $number
+            );
+        }
+    }
+
+    public function testPhoneNumberWithPrefix(): void
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $number = $this->faker->phoneNumber(format: '{d}{d}{d}', prefix: '+33');
+
+            $this->assertMatchesRegularExpression(
+                '/^\+33\d{3}$/',
+                $number
+            );
+        }
+
+        for ($i = 0; $i < 10; $i++) {
+            $number = $this->faker->phoneNumber('{d}{d}{d}{separator}{d}{d}{d}', '.', '0');
+
+            $this->assertMatchesRegularExpression(
+                '/^0\d{3}.\d{3}$/',
+                $number
+            );
+        }
+    }
+
+    public function testCellPhoneNumber(): void
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $number = $this->faker->cellPhoneNumber();
+
+            $this->assertMatchesRegularExpression(
+                '/^\d{4}$/',
+                $number
+            );
+        }
+    }
+
+    public function testCellPhoneNumberWithSeparator(): void
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $number = $this->faker->cellPhoneNumber('.');
+
+            $this->assertMatchesRegularExpression(
+                '/^\d{2}\.\d{2}$/',
+                $number
+            );
+        }
+    }
+
+    public function testCellPhoneNumberWithPrefix(): void
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $number = $this->faker->cellPhoneNumber(prefix: '+33');
+
+            $this->assertMatchesRegularExpression(
+                '/^\+33\d{4}$/',
+                $number
+            );
+        }
+    }
+
+    public function testLandlinePhoneNumber(): void
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $number = $this->faker->landlinePhoneNumber();
+
+            $this->assertMatchesRegularExpression(
+                '/^\d{8}|\d{10}$/',
+                $number
+            );
+        }
+    }
+
+    public function testLandlinePhoneNumberWithSeparator(): void
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $number = $this->faker->landlinePhoneNumber('.');
+
+            $this->assertMatchesRegularExpression(
+                '/^\d{2}\.\d{2}\.\d{2}\.\d{2}(\.\d{2})?$/',
+                $number
+            );
+        }
+    }
+
+    public function testLandlinePhoneNumberWithPrefix(): void
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $number = $this->faker->landlinePhoneNumber(prefix: '+33');
+
+            $this->assertMatchesRegularExpression(
+                '/^\+33\d{8}|\d{10}$/',
+                $number
+            );
+        }
+    }
+
+    public function testIndicatoredPhoneNumber(): void
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $number = $this->faker->indicatoredPhoneNumber();
+
+            $this->assertMatchesRegularExpression(
+                '/^00(\d{4}|\d{8}|\d{10})$/',
+                $number
+            );
+        }
+    }
+
+    public function testIndicatoredPhoneNumberWithFormat(): void
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $number = $this->faker->indicatoredPhoneNumber(format: '{d}{d}{d}{d}{d}{d}');
+
+            $this->assertMatchesRegularExpression(
+                '/^00(\d{6})$/',
+                $number
+            );
+        }
+    }
+
+    public function testIndicatoredPhoneNumberWithSeparator(): void
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $number = $this->faker->indicatoredPhoneNumber(format: '{d}{d}{separator}{d}{d}{separator}{d}{d}', separator: '-');
+
+            $this->assertMatchesRegularExpression(
+                '/^00(\d{2}-\d{2}-\d{2})$/',
+                $number
+            );
+        }
+    }
+
+    public function testIndicatoredCellPhoneNumber(): void
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $number = $this->faker->indicatoredCellPhoneNumber();
+
+            $this->assertMatchesRegularExpression(
+                '/^00\d{4}$/',
+                $number
+            );
+        }
+    }
+
+    public function testIndicatoredCellPhoneNumberWithSeparator(): void
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $number = $this->faker->indicatoredCellPhoneNumber('.');
+
+            $this->assertMatchesRegularExpression(
+                '/^00\d{2}\.\d{2}$/',
+                $number
+            );
+        }
+    }
+
+    public function testIndicatoredLandlinePhoneNumber(): void
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $number = $this->faker->indicatoredLandlinePhoneNumber();
+
+            $this->assertMatchesRegularExpression(
+                '/^00(\d{8}|\d{10})$/',
+                $number
+            );
+        }
+    }
+
+    public function testIndicatoredLandlinePhoneNumberWithSeparator(): void
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $number = $this->faker->indicatoredLandlinePhoneNumber('.');
+
+            $this->assertMatchesRegularExpression(
+                '/^00\d{2}\.\d{2}\.\d{2}\.\d{2}(\.\d{2})?$/',
                 $number
             );
         }
