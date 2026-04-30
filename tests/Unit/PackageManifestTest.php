b5394505d26eb9c62f43caf75ef20ceec29c5efe
Ayrton Andino Bonazzola <60512432+ayrtonandino@users.noreply.github.com>
2026-04-30 17:20:45 -0300

    add optimize-autoloader and fix namespaces for psr4

--- tests/Unit/PackageManifestTest.php
+++ tests/Unit/PackageManifestTest.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+namespace Xefi\Faker\Tests\Unit;
+
 use Xefi\Faker\Manifests\PackageManifest;
 use Xefi\Faker\Tests\Unit\TestCase;
 
--- tests/Unit/ProviderRepositoryTest.php
+++ tests/Unit/ProviderRepositoryTest.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+namespace Xefi\Faker\Tests\Unit;
+
 use Xefi\Faker\Tests\Unit\TestCase;
 
 final class ProviderRepositoryTest extends TestCase
